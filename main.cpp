9610dcba907dbee2014673b3f3df5ecddc789eda
AndreiFlorescu1410 <48299087+AndreiFlorescu1410@users.noreply.github.com>
2019-03-09 15:13:44 +0200

    ADDED: Eliminarea elementelor din k in k, Stergerea elementului 'x'

--- main.cpp
+++ main.cpp
@@ -26,7 +26,6 @@ public:
     friend istream& operator >>(istream& i, lista* c);
 
     lista();
-    ~lista();
 };
 
 lista::lista()
@@ -54,7 +53,7 @@ void insert_final(lista* &c, lista* new_element)
 {
     if (c == NULL)
         c = new_element;
-    else if (c->GetNext() == NULL)      //cazul in care nodul cap este singurlui din liste si trebuie formata lista circulara
+    else if (c->GetNext() == NULL)      //cazul in care nodul cap este singurul din lista si trebuie formata lista circulara
     {
         c->SetNext(new_element);        //formez conexiune tip "cerc"
         c->SetPrev(new_element);
@@ -84,17 +83,40 @@ void insert_after_x(lista* c,lista* new_element, int x)
     after_new->SetPrev(new_element);
 }
 
-void read(lista* &c, int &n) //meniul de citire
+void remove_x(lista* &first,lista* c, int x) // 'first' este primul nod, 'c' este nodul de unde vrem sa incepem cautarea si 'x' valoarea pe care vrem sa o stergem
 {
+    while(c->GetI() != x)            //caut nodul cu valorea x
+        c = c->GetNext();
+    if(first == c)                   //cazul in care vrem sa stergem primul nod
+        first = c->GetNext();
+    (c->GetNext())->SetPrev(c->GetPrev());
+    (c->GetPrev())->SetNext(c->GetNext());
+    delete c;
+}
+
+void remove_k(lista* &first, lista* c, int &n, int k) //argumente similare cu 'remove_x', insa, in loc de valoarea x, avem distanta dintre elemente si numarul de elemente din lista
+{
+    while (n > 1) //cat timp mai sunt noduri de sters
+    {
+        remove_x(first,c,c->GetI()); //sterg nodul curent
+        n--;
+        for(int i = 0; i < k; i++)  //avansez k noduri
+            c = c->GetNext();
+    }
+
+}
+int read(lista* &c, int &n, bool reading_list) //meniul de citire. Foloesesc reading_list sa stiu daca citesc lista sau elementul 'k'
+{
+    int k;
     antet
-    cout << "Alegeti metoda de citire a listei: " << endl;
+    cout << "Alegeti metoda de citire a" << ((reading_list == 1)?" listei":" a lui k") << endl; //afisare in functie de ce citesc
     cout << "1. Fisier" << endl;
     cout << "2. Tastatura" << endl;
     int caz;
     cin >> caz;
     switch (caz)
     {
-    case 1:
+    case 1:  //fisier
     {
         while  (1)
         {
@@ -110,37 +132,50 @@ void read(lista* &c, int &n) //meniul de citire
                 cout<<"Apasa orice tasta pentru a te intoarce..."<<endl;
                 getch();
             }
-            else
+            else                    //am putut sa deschid fisierul
             {
-                lista* new_element = c;
-                c = NULL;
-                while(f>>new_element)           //citesc valoarea nodului
+                if (reading_list == 1)
                 {
-                    insert_final(c,new_element);  //il adaug la "finalul" listei
-                    new_element = new lista;    //reinitializez noul element
-                    n++;                        //incrementez numarul nodurilor din lista
+                    lista* new_element = c;
+                    c = NULL;
+                    while(f>>new_element)             //citesc valoarea nodului
+                    {
+                        insert_final(c,new_element);  //il adaug la "finalul" listei
+                        new_element = new lista;      //reinitializez noul element
+                        n++;                          //incrementez numarul nodurilor din lista
+                    }
                 }
+                else
+                    f >> k;
                 break;
 
             }
         }
         break;
     }
-    case 2:
+    case 2: //tastatura
     {
         while (1)
         {
             antet
-            cout << "Dati numarul de elemente din lista: ";
-            cin >> n;
-            cout << "Dati elementele listei: ";
-            lista* new_element = c;
-            c = NULL;
-            for (int i = 0; i < n; i++)
+            if (reading_list == 1)
             {
-                cin >> new_element;         //citesc valoarea nodului
-                insert_final(c,new_element);  //il adaug la "finalul" listei
-                new_element = new lista;    //reinitializez noul element
+                cout << "Dati numarul de elemente din lista: ";
+                cin >> n;
+                cout << "Dati elementele listei: ";
+                lista* new_element = c;
+                c = NULL;
+                for (int i = 0; i < n; i++)
+                {
+                    cin >> new_element;           //citesc valoarea nodului
+                    insert_final(c,new_element);  //il adaug la "finalul" listei
+                    new_element = new lista;      //reinitializez noul element
+                }
+            }
+            else
+            {
+                cout << "Dati valoarea lui k: ";
+                cin >> k;
             }
             break;
 
@@ -154,15 +189,20 @@ void read(lista* &c, int &n) //meniul de citire
         cout<<"Optiune inexistenta."<<endl;
         cout<<"Apasa orice tasta pentru a te intoarce..."<<endl;
         getch();
-        read(c,n);
+        read(c,n,(reading_list == 1)?1:0); //ma reintorc la meniul principal
         break;
     }
     }
+    if (reading_list == 1)
+        return 0;
+    else
+        return k;
 }
 
 void afisare(lista* first, int n) //afiseaz lista care incepe de la nodul "first"
 {
     antet
+    cout << "Lista are " << n << " elemente: " << endl;
     for(int i = 0; i < n; i++)
     {
         cout << first;
@@ -172,6 +212,9 @@ void afisare(lista* first, int n) //afiseaz lista care incepe de la nodul "first
     getch();
 }
 
+
+
+
 void menu(lista* first, int n) //meniul principal al programului
 {
     antet
@@ -179,7 +222,7 @@ void menu(lista* first, int n) //meniul principal al programului
     cout << "1. Afiseaza lista" << endl;
     cout << "2. Adauga un nou element" << endl;
     cout << "3. Sterge un element" << endl;
-    cout << "4. Inverseaza legaturile listei" << endl;
+    cout << "4. Inverseaza legaturile listei (??)" << endl;
     cout << "5. Elimina elemente din k in k" << endl;
     cout << "6. Concateneaza lista curenta cu una noua" << endl;
     cout << "9. Iesi din program" << endl;
@@ -243,15 +286,33 @@ void menu(lista* first, int n) //meniul principal al programului
         if (caz2 == 1 || caz2 == 2) //conditie necesare pentru a iesii din loop-ul infinit
             break;
         }
+        break;
     }
     case 3:
     {
-
+        antet
+        int x;
+        cout << "Dati valoarea nodului pe care vreti sa-l stergeti: "; cin>>x;
+        remove_x(first, first, x);
+        n--;
+        cout << endl << "Nod sters cu succes." << endl ;
+        cout<<endl<<"Apasa orice tasta pentru a te intoarce..."<<endl;
+        getch();
+        break;
+    }
+    case 5:
+    {
+        int k=read(first,n,0);
+        cout<<k;
+        remove_k(first,first,n,k);
+        cout << endl << "Noduri sterse cu succes." << endl ;
+        cout<<endl<<"Apasa orice tasta pentru a te intoarce..."<<endl;
+        getch();
+        break;
     }
 
 
     }
-    getch();
     menu(first, n);
 }
 
@@ -264,7 +325,7 @@ int main()
     int x,n=0,caz;
     lista *c  = new lista, *new_element = new lista, *first = c;
 
-    read(c,n);
+    read(c,n,1);
     menu(first, n);
 
 
